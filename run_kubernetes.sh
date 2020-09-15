593dd6562fdb547573d4c20c38c8d1e114eb2a99
jingxian <jingxianlin@chinamobile.com>
2020-09-15 15:00:23 -0700

    Push work to Github repository

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=jensenlin/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api --image=$dockerpath --port=80
+sleep 30
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/api 8080:80
